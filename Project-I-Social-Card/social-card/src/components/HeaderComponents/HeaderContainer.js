605c8be90565736f42e91108ee532e1aa8616503
ghungpham <pham.ghungj@gmail.com>
2019-04-22 13:58:57 -0400

    MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './Header.css';
+import App from '../../App';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+const HeaderContainer = () => {
+ return (
+     <div className = "HeaderContainer">
+     <div className = "Logo">
+     <ImageThumbnail />
+     </div>
+     <div className = "TitleContent">
+     <HeaderTitle />
+     <HeaderContent />
+     </div>
+     </div>
+ );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return(
+        <div className = "HeaderTitle">
+        <h1>Lambda School</h1>
+        <h2>@LambdaSchool · 22 apr</h2>
+        </div>
+    )
+};
+
+export default HeaderTitle;
