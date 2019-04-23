9a52d1f493c95b5f86be4710b515c342fcdb8769
ghungpham <pham.ghungj@gmail.com>
2019-04-23 16:56:57 -0400

    MVP

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const actions = ["÷", "x", "+", "-", "="];
+const ActionButton = () => {
+    return (
+        <div className = "actionbuttons">
+        {actions.map(action => { return <div className ="action">{action}</div>})}
+        </div>
+)
+};
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Button.css';
+
+
+const Numbers = [1,2,3,4,5,6,7,8,9]
+const NumberButton = () => {
+return (
+    <div className = "numbers">
+    <div className = "clear">clear</div>
+    <div className = "nonzero">
+    {Numbers.map(number => {return <div className="number">{number}</div>})}
+    </div>
+    <div className ="zero">0</div>
+    </div>
+)
+};
+
+export default NumberButton;
